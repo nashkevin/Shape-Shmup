716c368b2d747631454261546907a60e2f006edf
Drew Mitchell <hi@callmedrew.com>
2016-10-18 21:26:32 -0400

    Add key listeners on cardinal directions (WASD and arrow keys).

--- src/main/webapp/game.js
+++ src/main/webapp/game.js
@@ -2,7 +2,7 @@ var webSocket;
 var messages = document.getElementById("messages");
 
 
-function openSocket(){
+function openSocket() {
 	// Ensures only one connection is open at a time
 	if(webSocket !== undefined && webSocket.readyState !== WebSocket.CLOSED) {
 		writeResponse("WebSocket is already opened.");
@@ -13,34 +13,53 @@ function openSocket(){
 	webSocket = new WebSocket(url);
 	 
 	// Binds functions to the listeners for the websocket.
-	webSocket.onopen = function(event){
+	webSocket.onopen = function(event) {
 		if(event.data === undefined) 
 			return;
 
 		writeResponse(event.data);
 	};
 
-	webSocket.onmessage = function(event){
+	webSocket.onmessage = function(event) {
 		writeResponse(event.data);
 	};
 
-	webSocket.onclose = function(event){
+	webSocket.onclose = function(event) {
 		writeResponse("Connection closed");
 	};
 }
 
-/**
- * Sends the value of the text input to the server
- */
-function send(){
+// Sends the value of the text input to the server
+function send() {
 	var text = document.getElementById("messageinput").value;
 	webSocket.send(text);
 }
 
-function closeSocket(){
+function closeSocket() {
 	webSocket.close();
 }
 
-function writeResponse(text){
+function writeResponse(text) {
 	messages.innerHTML += "<br/>" + text;
 }
+
+// Key listener
+window.onkeydown = function (e) {
+	if (document.getElementById("messageinput") !== document.activeElement) {
+		var code = e.keyCode ? e.keyCode : e.which;
+		switch (code) {
+			case 87: case 38:  // 'w' or up
+				webSocket.send("Up");
+				break;
+			case 65: case 37: // 'a' or left 
+				webSocket.send("Left");
+				break;
+			case 83: case 40: // 's' or down
+				webSocket.send("Down");
+				break;
+			case 68: case 39: // 'd' or right
+				webSocket.send("Right");
+				break;
+		}
+	}
+};
