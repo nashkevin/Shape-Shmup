6c4e85c6f731e75cd3474600f2d4e319bbdd599f
Kevin Wang <kevinptn@gmail.com>
2016-10-26 21:42:56 -0400

    Added JUnit

--- src/main/java/junit/PlayerAgentTest.java
+++ src/main/java/junit/PlayerAgentTest.java
@@ -1,1 +1,12 @@
-//TODO
+package main.java.junit;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class PlayerAgentTest {
+	@Test
+	public void testConstructor() {
+		//TODO
+		Assert.fail();
+	}
+}
