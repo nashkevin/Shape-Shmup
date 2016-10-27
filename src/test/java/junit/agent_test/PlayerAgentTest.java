b18a5f410626dbebe56a3e80e19eb65915c6df9c
Kevin Wang <kevinptn@gmail.com>
2016-10-27 14:15:59 -0400

    Added constuctor test for PlayerAgent

--- src/test/java/junit/agent_test/PlayerAgentTest.java
+++ src/test/java/junit/agent_test/PlayerAgentTest.java
@@ -1,16 +1,33 @@
 package test.java;
-
+import main.java.agent.PlayerAgent;
+import main.java.environment.Environment;
 import junit.framework.Assert;
 import org.junit.Test;
-
+import java.awt.Point;
+import java.util.UUID;
 public class PlayerAgentTest {
 	@Test
 	public void testConstructor() {
-		//TODO	
+		Environment environment = new Environment(20);
+		PlayerAgent player = new PlayerAgent(UUID.randomUUID(), environment, new Point(10, 10), 1, 1, 100, 10, 1, 1);
+
+		Assert.assertEquals(new Point(10,10), player.getPosition());
+		Assert.assertEquals(1, player.getLevel());
+		Assert.assertEquals(1, player.getTeam());
+		Assert.assertEquals(100, player.getHealth());
+		Assert.assertEquals(10, player.getDamage());
+
+
 	}
 	@Test
-	public void testEqual() {
-		Assert.assertTrue(true);
-		Assert.assertTrue(true);
+	public void testAddPlayerEvent() {
+		ClientInput event = new ClientInput();
+		event.setDirection("left");
+
+		player.addPlayerEvent(event);
+
 	}
+
+
+
 }
