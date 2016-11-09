92e9c73a6e27502ee301512c6c7f36f4208700f6
Zach Janice <znj@case.edu>
2016-11-08 19:45:56 -0500

    Unit tests for Agent class, mostly done

---
+++ src/test/java/junit/mocks_and_imps/EnvironmentMock.java
@@ -0,0 +1,10 @@
+package test.java.junit.mocks_and_imps;
+
+import main.java.environment.Environment;
+
+public class EnvironmentMock extends Environment {
+	public EnvironmentMock(double radius) {
+		super(radius);
+	}
+
+}
