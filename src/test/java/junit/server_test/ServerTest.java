4958ba56656df1b2182ff0fffb6c2a8e35bbba73
Drew Mitchell <hi@callmedrew.com>
2016-11-08 19:38:46 -0500

    Add simple tests for the web server.

--- src/test/java/junit/server_test/ServerTest.java
+++ src/test/java/junit/server_test/ServerTest.java
@@ -3,6 +3,7 @@ package test.java.junit.server_test;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.websocket.RemoteEndpoint;
 import javax.websocket.Session;
@@ -25,16 +26,53 @@ public class ServerTest {
 	}
 	
 	@Test
-	public void testConnectionOpen() {
-		LinkedList<String> output = new LinkedList<String>();
-		Session session = getMockSession("test", output);
+	public void testSimpleConnection() {
+		List<String> output = new LinkedList<String>();
+		Session session = getMockSession(output);
 		
 		server.onOpen(session);
+		
+		String name = " {\"name\":\"Test\"}";
+		server.onMessage(name, session);
+		
+		server.onClose(session);
 	}
 	
-	private Session getMockSession(String id, List<String> output) {
+	@Test
+	public void testChatMessage() {
+		// Instantiate two mock sessions.
+		List<String> output1 = new LinkedList<String>();
+		List<String> output2 = new LinkedList<String>();
+		Session session1 = getMockSession(output1);
+		Session session2 = getMockSession(output2);
+		
+		// Connect them to the server.
+		server.onOpen(session1);
+		server.onOpen(session2);
+		
+		// Name the first session.
+		String name = " {\"name\":\"Test1\"}";
+		server.onMessage(name, session1);
+		
+		// Send a chat message from the first.
+		String message = " {\"message\":\"Message.\"}";
+		server.onMessage(message, session1);
+
+		// Verify that both clients receive the chat message.
+		String receivedMessage = "<strong>Test1:</strong> Message.";
+		Assert.assertTrue(output1.contains(receivedMessage));
+		Assert.assertTrue(output2.contains(receivedMessage));
+		
+		server.onClose(session1);
+		server.onClose(session2);
+	}
+	
+	private Session getMockSession(List<String> output) {
 		Session session = Mockito.mock(Session.class);
-		Mockito.when(session.getId()).thenReturn(id);
+		String uuid = UUID.randomUUID().toString();
+		Mockito.when(session.getId()).thenReturn(uuid);
+		
+		Mockito.when(session.isOpen()).thenReturn(true);
 		
 		Mockito.when(session.getBasicRemote()).thenAnswer(new Answer<RemoteEndpoint.Basic>() {
 			public RemoteEndpoint.Basic answer(InvocationOnMock invocation) throws Throwable {
