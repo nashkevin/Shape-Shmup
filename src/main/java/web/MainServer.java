e187c923a1ba4b64cc909684ead8f2773396b05a
Drew Mitchell <hi@callmedrew.com>
2016-12-09 13:19:37 -0500

    Fix whitespace on MainServer.

--- src/main/java/web/MainServer.java
+++ src/main/java/web/MainServer.java
@@ -7,26 +7,26 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 public class MainServer {
 	public static void main(String[] args) {
-        Server server = new Server();
-        ServerConnector connector = new ServerConnector(server);
-        connector.setPort(8080);
-        server.addConnector(connector);
+		Server server = new Server();
+		ServerConnector connector = new ServerConnector(server);
+		connector.setPort(8080);
+		server.addConnector(connector);
 
-        // Setup the basic application "context" for this application at "/"
-        // This is also known as the handler tree (in jetty speak)
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        
-        ServletHolder holder = new ServletHolder("socket", WebServlet.class);
-        context.addServlet(holder, "/socket/*");
+		// Setup the basic application "context" for this application at "/"
+		// This is also known as the handler tree (in jetty speak)
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
 
-        try {
-            server.start();
-            server.dump(System.err);
-            server.join();
-        } catch (Throwable t) {
-            t.printStackTrace(System.err);
-        }
-    }
+		ServletHolder holder = new ServletHolder("socket", WebServlet.class);
+		context.addServlet(holder, "/socket/*");
+
+		try {
+			server.start();
+			server.dump(System.err);
+			server.join();
+		} catch (Throwable t) {
+			t.printStackTrace(System.err);
+		}
+	}
 }
